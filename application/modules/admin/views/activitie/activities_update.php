9df7045d4d89b8c2c98d522125e2f8bfb6b5bf7c
Giovanni H. Huka <41690121+giovannihuka@users.noreply.github.com>
2019-12-05 11:36:21 +0700

    Update activities_update.php

--- application/modules/admin/views/activitie/activities_update.php
+++ application/modules/admin/views/activitie/activities_update.php
@@ -6,17 +6,36 @@
         <div class="box box-primary">
             <div class="box-body">
                 <?php echo $form->open(); ?>
-				<?php echo $form->bs3_dropdown('Nama Kegiatan','ref_activityid',$activity_list,$activitie['ref_activityid'],'','Pilih Nama Kegiatan'); ?>
-                <?php echo $form->bs3_date('Tgl. Kegiatan','activity_date',$activitie['activity_date'],'','','Tanggal Kegiatan'); ?>
-                <?php echo $form->bs3_textarea('Keterangan','remarks',$activitie['remarks'],'','','Keterangan'); ?>
-                <script>
-                    window.onload = function() {
-                        CKEDITOR.replace('remarks',{});
-                    };
-                </script>
-				<?php echo $form->bs3_time('Mulai Kegiatan','time_start',$activitie['time_start'],'','','Masukkan Mulai Kegiatan'); ?>
-				<?php echo $form->bs3_time('Selesai Kegiatan','time_end',$activitie['time_end'],'','','Masukkan Selesai Kegiatan'); ?>
-				<?php echo $form->bs3_dropdown('Status Data','status_data',$status_list,$activitie['status_data'],'','Pilih Status Data'); ?>
+                <div class="row">
+                    <div class="col-md-9">
+				        <?php echo $form->bs3_dropdown('Nama Kegiatan','ref_activityid',$activity_list,$activitie['ref_activityid'],'','Pilih Nama Kegiatan'); ?>
+                    </div>
+                    <div class="col-md-3">
+                        <?php echo $form->bs3_date('Tgl. Kegiatan','activity_date',$activitie['activity_date'],'','','Tanggal Kegiatan'); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo $form->bs3_textarea('Keterangan','remarks',$activitie['remarks'],'','','Keterangan'); ?>
+                        <script>
+                            window.onload = function() {
+                                CKEDITOR.replace('remarks',{});
+                            };
+                        </script>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+				        <?php echo $form->bs3_time('Mulai Kegiatan','time_start',$activitie['time_start'],'','','Masukkan Mulai Kegiatan'); ?>
+                    </div>
+                    <div class="col-md-4">
+				        <?php echo $form->bs3_time('Selesai Kegiatan','time_end',$activitie['time_end'],'','','Masukkan Selesai Kegiatan'); ?>
+                    </div>
+                    <div class="col-md-4">
+                        <?php echo $form->bs3_dropdown('Status Data','status_data',$status_list,$activitie['status_data'],'','Pilih Status Data'); ?>
+                    </div>
+                </div>
+				
 				<?php echo $form->bs3_submit('Update'); ?>
             	<?php echo '<button type="reset" class="btn btn-default" onclick="location.href=\'activitie\'">Back to List</button>' ?>
             	<?php echo $form->close(); ?>
